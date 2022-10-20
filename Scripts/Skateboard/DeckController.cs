e95d5d59ecd442a6414652b63fe3c47a3fe2c420
Jamie Bailey <psndanjam123@gmail.com>
2022-10-20 20:40:32 +0100

    setup baseplate colliders

---
+++ Scripts/Skateboard/DeckController.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonSkate.Skateboard
+{
+
+    public class DeckController : UdonSharpBehaviour
+    {
+        private Rigidbody _rigidbody;
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+    }
+}
