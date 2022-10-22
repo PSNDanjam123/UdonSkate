45dbf0cb63e61dc2ec8433872e93c98048ffaf61
Jamie Bailey <psndanjam123@gmail.com>
2022-10-22 18:26:21 +0100

    custom skateboard collider working

---
+++ Scripts/Skateboard/DeckController.cs
@@ -0,0 +1,94 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonSkate.Skateboard
+{
+    public class DeckController : UdonSharpBehaviour
+    {
+        public WheelController[] wheels;
+
+        private Rigidbody rb;
+
+        private bool grounded = false;
+
+        void Start()
+        {
+            _init();
+        }
+
+
+        void Update()
+        {
+            _setGrounded();
+            if (grounded)
+            {
+                Vector3 normal = _calculateNormal();
+                _applySurfaceForce(normal);
+                Vector3 forwardRot = _calculateForwardRotation(normal);
+                Debug.DrawLine(transform.position, transform.position - forwardRot, Color.magenta);
+                rb.rotation = Quaternion.LookRotation(forwardRot, normal);
+            }
+        }
+
+        private void _init()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void _applySurfaceForce(Vector3 normal)
+        {
+            rb.AddForce(-Vector3.Project(rb.velocity, -normal), ForceMode.Impulse);
+            rb.AddForce(-Vector3.Project(Physics.gravity, -normal), ForceMode.Acceleration);
+
+            if (Physics.Raycast(transform.position, -normal, out RaycastHit hitInfo, 0.3f))
+            {
+                if (Vector3.Distance(transform.position, hitInfo.point) > 0.2f)
+                {
+                    rb.AddForce(-Vector3.Project(Physics.gravity, -normal), ForceMode.Acceleration);
+                }
+            }
+        }
+
+        private Vector3 _calculateForwardRotation(Vector3 normal)
+        {
+            return Vector3.Cross(Vector3.Cross(normal, transform.forward), normal);
+
+        }
+
+        private Vector3 _calculateNormal()
+        {
+            Vector3 normal = Vector3.zero;
+            foreach (var wheel in wheels)
+            {
+                if (!wheel.Collision)
+                {
+                    continue;
+                }
+                normal += wheel.HitInfo.normal;
+
+            }
+            if (normal == Vector3.zero)
+            {
+                normal = transform.up;
+            }
+            return normal;
+        }
+
+        private void _setGrounded()
+        {
+            foreach (var wheel in wheels)
+            {
+                if (wheel.Collision)
+                {
+                    grounded = true;
+                    return;
+                }
+
+            }
+            grounded = false;
+        }
+    }
+}
--- Scripts/Skateboard/WheelController.cs
+++ Scripts/Skateboard/WheelController.cs
@@ -4,10 +4,53 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-public class WheelController : UdonSharpBehaviour
+namespace UdonSkate.Skateboard
 {
-    void Start()
+    public class WheelController : UdonSharpBehaviour
     {
-        
+        public DeckController deck;
+        public float radius;
+
+        private RaycastHit hitInfo;
+        private bool collision = false;
+
+        private Rigidbody rb;
+
+        void Start()
+        {
+            _init();
+        }
+
+        void Update()
+        {
+            _raycast();
+        }
+
+        public bool Collision
+        {
+            get
+            {
+                return collision;
+            }
+        }
+
+        public RaycastHit HitInfo
+        {
+            get
+            {
+                return hitInfo;
+            }
+        }
+        private void _init()
+        {
+            rb = deck.GetComponent<Rigidbody>();
+        }
+
+        private void _raycast()
+        {
+            collision = Physics.Raycast(transform.position, -transform.up, out hitInfo, radius);
+            Debug.DrawLine(transform.position, transform.position + (-transform.up * radius), Color.white);
+        }
+
     }
 }
